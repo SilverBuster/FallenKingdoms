c35966dc8979391aadf3ecaf192f8553add7bb0c
SilverBuster <mael@morin.fr>
2015-11-01 18:53:27 +0100

    Nouveau fichier

---
+++ src/fr/morin/fallenkingdoms/FallenKingdoms.java
@@ -0,0 +1,14 @@
+package fr.morin.FallenKingdoms;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FallenKingdoms extends JavaPlugin {
+	public static void main(String[] args) {
+	}
+
+	public void onEnable() {
+	}
+
+	public void onDisable() {
+	}
+}
